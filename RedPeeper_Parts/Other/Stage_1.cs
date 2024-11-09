be045c1c8a24e932bfcb0b1335521dd57ca0ea30
Олег Шипов <shipovoleg2007@gmail.com>
2024-11-09 13:57:33 +0300

    Новый фон

---
+++ RedPeeper_Parts/Other/Stage_1.cs
@@ -0,0 +1,15 @@
+﻿using Nautilus.Handlers;
+using Nautilus.Utility;
+using System.IO;
+using System.Reflection;
+
+public class Stage_1
+{
+    public static string modFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    public static string bgPath = Path.Combine(modFolder, "Assets", "BG", "Stage_1.png");
+    private void Awake()
+    {
+    var myCustomBackground = EnumHandler.AddEntry<CraftData.BackgroundType>("Stage_1")
+                .WithBackground(ImageUtils.LoadSpriteFromFile(bgPath));
+    }
+}
