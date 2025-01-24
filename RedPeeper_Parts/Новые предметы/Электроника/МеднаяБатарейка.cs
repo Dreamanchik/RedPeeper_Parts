5e1d1f06047adfd95b3246b1bd43ce20ce92195e
Олег Шипов <shipovoleg2007@gmail.com>
2025-01-24 18:09:34 +0300

    Медная батарейка создана

---
+++ RedPeeper_Parts/Новые предметы/Электроника/МеднаяБатарейка.cs
@@ -0,0 +1,59 @@
+﻿using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Assets;
+using System.IO;
+using System.Reflection;
+using Nautilus.Utility;
+using Nautilus.Crafting;
+using static CraftData;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Extensions;
+using UnityEngine;
+
+public class МеднаяБатарейка
+{
+    //    ЧТОБЫ ПРЕДМЕТ МОЖНО БЫЛО ИСПОЛЬЗОВАТЬ ГДЕ УГОДНО. ДЛЯ ЭТОГО НУЖНО ПРОСТО ВПИСАТЬ (Название класса).Info.(Название функции. Например, TechType). КАК ПРИМЕР - RedPeeper_CyclopsEngine.Info.TechType
+    public static PrefabInfo Info { get; private set; }
+
+    //    ИКОНКА
+    public static string modFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    public static string iconPath = Path.Combine(modFolder, "Assets", "Items", "Electronics", "CopperBattery.png"); // <-- Заменить на нужное. ОГРОМНОЕ ЖЕЛАНИЕ ПАПКИ ДЕЛАТЬ ТАКИМИ ЖЕ КАК И В ПРОЕКТЕ. Структура должна совпадать с папками в моде. Тоесть, если иконка просто находится в папке Assets, то код будет выглядеть как <<"Assets", "CyclopsEngine.png">>, а если находится с папке Assets и потом в папке Items, потом Cyclops и потом Objects, то <<"Assets", "Items", "Cyclops", "Objects", "CyclopsEngine.png">>
+
+    public static string texturePath = Path.Combine(modFolder, "Assets", "Items", "Advanced Materials", "TwoFactorProcessor_texture.png"); //  <-- Путь к текстуре
+    public static Texture2D Texture = ImageUtils.LoadTextureFromFile(texturePath);
+    public static void Register()
+    {
+        Info = PrefabInfo.WithTechType(
+            //    АЙДИ, НАЗВАНИЕ, ОПИСАНИЕ
+            "RedPeeper_CopperBattery",
+            "Медная батарея",
+            "Портативный и мобильный аккумулятор, позволяющий запитать персональные инструменты. Пониженная энергоёмкость."
+            )
+            .WithIcon(ImageUtils.LoadSpriteFromFile(iconPath))
+            .WithSizeInInventory(new Vector2int(1, 1)); // РАЗМЕР В ИНВЕНТАРЕ
+        //CraftDataHandler.SetBackgroundType(Info.TechType, CraftData.BackgroundType.ExosuitArm); // ФОН
+
+        var _prefab = new CustomPrefab(Info);
+
+        var _obj = new CloneTemplate(Info, TechType.Battery); // КОПИРУЕМ ПРЕФАБ НА ОСНОВЕ ТЕЧТАЙПА
+        _obj.ModifyPrefab += obj =>
+        {
+            //MeshRenderer mr = obj.transform.Find("model").gameObject.transform.Find("Mesh").gameObject.GetComponent<MeshRenderer>();
+            //mr.material.mainTexture = Texture;
+            //mr.material.SetTexture(ShaderPropertyID._Diffusion, Texture);
+            Battery batteryComponent = obj.GetComponent<Battery>();
+            batteryComponent._capacity = 50;
+        };
+        _prefab.SetGameObject(_obj);
+        _prefab.SetRecipe(new RecipeData(
+            //    РЕЦЕПТ НАЧАЛО
+            new Ingredient(TechType.Titanium)
+            //    РЕЦЕПТ КОНЕЦ
+            ))
+            .WithFabricatorType(CraftTree.Type.Fabricator)
+            .WithStepsToFabricatorTab("Resources", "BasicMaterials")
+            .WithCraftingTime(5f); // ВРЕМЯ КРАФТА
+        _prefab.SetPdaGroupCategory(TechGroup.Resources, TechCategory.BasicMaterials); // МЕСТОНАХОЖДЕНИЕ В КПК
+
+        _prefab.Register(); // РЕГИСТРАЦИЯ ОБЪЕКТА. ПОСЛЕ ЭТОГО НИЧЕГО НЕ ПИШЕМ
+    }
+}
